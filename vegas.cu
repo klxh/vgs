6e34173d7a0e6446559e8ededea2ca80dabb8a6a
Klejdis Xhani <klejdisxhani@jacobi.pcteor1.mi.infn.it>
2020-06-17 18:15:19 +0200

    definite griglia di integrazione e griglia delle spaziature

--- vegas.cu
+++ vegas.cu
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define vegas_cycles 1
-#define dim 2
+#define vegas_cycles 1 // numero di iterazioni vegas
+#define dim 2 // numero di variabili della funzione integranda
+#define Nc 10 // numero di suddivisioni degli intervalli di integrazione
+
+
 // definizione della funzione integranda
-float f(float r[dim])
+double f(double r[dim])
 {
 	return r[0] + r[1];	
 }
@@ -12,12 +15,28 @@ float f(float r[dim])
 
 int main ()
 {
+	// definizione della griglia di integrazione e inizializzazione con equispaziature
+	double grid[dim * (Nc + 1)];
+	for(int i = 0; i < dim; i++)
+	{
+		grid[i * (Nc + 1)] = 0;
+		printf("grid[%d][%d] = %f\n", i, 0, grid[i * (Nc + 1)]);
+		for(int j = 1; j < Nc + 1; j++)
+		{
+			grid[i * (Nc + 1) + j] = grid[i * (Nc + 1) + j - 1] + 1./Nc;
+		  printf("grid[%d][%d] = %f\n", i, j, grid[i * (Nc + 1) + j]);
+		}
+	}
+
+	// definizione della griglia delle spaziature
+	double spacings[dim * Nc];
+	for(int i = 0; i < dim * Nc; i++) { spacings[i] = 1./Nc; }
+
+
 	// cicli vegas
 	for (int it = 0; it < vegas_cycles; it++)
 	{
 		printf("###### ITERAZIONE VEGAS %d ######\n\n", it);
-		float r[2] = {1, 2};
-		printf("f(1, 2) = %f\n", f(r));
 	}
 	
 	return 0;
