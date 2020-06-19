ac877c1e64bb7078ce3f6d2880183d63d91bb72a
Klejdis Xhani <klejdisxhani@jacobi.pcteor1.mi.infn.it>
2020-06-19 20:12:40 +0200

    Copiato generatore Mersenne Twister in vegas.cu

--- vegas.cu
+++ vegas.cu
@@ -13,7 +13,10 @@
 #define vegas_cycles 1 // numero di iterazioni vegas
 #define dim 2 // numero di variabili della funzione integranda
 #define Nc 10 // numero di suddivisioni degli intervalli di integrazione
-
+#define NTHREADS 256
+#define NBLOCKS 64
+#define RNG_MAX 4294967295 
+#define DEFAULT_SEED 5234
 
 #define CUDA_CALL(x) do { if((x) != cudaSuccess) { \
     printf("Error at %s:%d\n",__FILE__,__LINE__); \
@@ -23,26 +26,13 @@
     printf("Error at %s:%d\n",__FILE__,__LINE__); \
     return EXIT_FAILURE;}} while(0)
 
-__global__ void generate_kernel(curandStateMtgp32 *state, 
-                                int n,
-                                int *result)
+		
+__global__ void generate_kernel(curandStateMtgp32 *state, double *result)
 {
-    int id = threadIdx.x + blockIdx.x * gridDim.x;
-    int count = 0;
-    unsigned int x;
+    int id = threadIdx.x + blockIdx.x * NTHREADS;
     /* Generate pseudo-random unsigned ints */
-    for(int i = 0; i < n; i++) {
-        x = curand(&state[blockIdx.x]);
-        /* Check if low bit set */
-        if(x & 1) {
-            count++;
-        }
-    }
-    /* Store results */
-    result[id] += count;
+    result[id] = (double) curand(&state[blockIdx.x]) / RNG_MAX;
 }
-
-
 
 // definizione della funzione integranda
 float f(float r[dim])
@@ -53,6 +43,8 @@ float f(float r[dim])
 
 int main ()
 {
+	int seed = DEFAULT_SEED;
+
 	// definizione della griglia di integrazione e inizializzazione con equispaziature
 	float grid[dim * (Nc + 1)];
 	for(int i = 0; i < dim; i++)
@@ -70,8 +62,40 @@ int main ()
 	float spacings[dim * Nc];
 	for(int i = 0; i < dim * Nc; i++) { spacings[i] = 1./Nc; }
 
+	// set up per generazione di numeri random
   curandStateMtgp32 *devMTGPStates;
   mtgp32_kernel_params *devKernelParams;
+	
+	double *dev_results;
+
+  /* Allocate space for prng states on device */
+  CUDA_CALL(cudaMalloc((void **)&devMTGPStates, NBLOCKS * sizeof(curandStateMtgp32)));
+ 
+
+  /* Setup MTGP prng states */
+  
+  /* Allocate space for MTGP kernel parameters */
+  CUDA_CALL(cudaMalloc((void**)&devKernelParams, sizeof(mtgp32_kernel_params)));
+  
+  /* Reformat from predefined parameter sets to kernel format, */
+  /* and copy kernel parameters to device memory               */
+  CURAND_CALL(curandMakeMTGP32Constants(mtgp32dc_params_fast_11213, devKernelParams));
+  
+  /* Initialize one state per thread block */
+  CURAND_CALL(curandMakeMTGP32KernelState(devMTGPStates, 
+              mtgp32dc_params_fast_11213, devKernelParams, NBLOCKS, seed));
+  
+	CURAND_CALL(cudaMalloc((void**)&dev_results, NBLOCKS * NTHREADS * sizeof(double)));
+
+  /* State setup is complete */
+  
+	generate_kernel<<<NBLOCKS,NTHREADS>>>(devMTGPStates, dev_results);
+
+	double host_results[NBLOCKS * NTHREADS];
+
+  CUDA_CALL(cudaMemcpy(host_results, dev_results, NBLOCKS * NTHREADS * sizeof(double), cudaMemcpyDeviceToHost));
+
+	for(int i = 0; i < NBLOCKS * NTHREADS; i++) { printf("host_result[%d] = %f\n", i, host_results[i]); }
 
 	// cicli vegas
 	for (int it = 0; it < vegas_cycles; it++)
@@ -79,5 +103,10 @@ int main ()
 		printf("\n###### ITERAZIONE VEGAS %d ######\n\n", it);
 	}
 	
-	return 0;
+  CUDA_CALL(cudaFree(devMTGPStates));
+  CUDA_CALL(cudaFree(dev_results));
+
+ 	return EXIT_SUCCESS;
 }
+
+
